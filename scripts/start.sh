20a64f044382fbed5db479bea113469850704f7a
heechang.kwon <facee22av@naver.com>
2021-08-27 00:18:44 +0900

    modify start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,8 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=freelec-springboot2-webservice
+REPOSITORY=/home/ec2-user/shoppingApp/step1
+PROJECT_NAME=springboot-shoppingmall-project
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
@@ -27,6 +27,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/shoppingApp/application-oauth.properties,/home/ec2-user/shoppingApp/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
