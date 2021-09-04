fc79e43d7eb2627af8fa5c6280550e5eab691024
heechang.kwon <facee22av@naver.com>
2021-09-04 16:07:39 +0900

    cart 총 가격 출력 기능 추가

---
+++ src/main/resources/static/js/carts.js
@@ -0,0 +1,14 @@
+carts = document.querySelectorAll("#cart");
+var sum = 0
+carts.forEach((el) => {
+    price = el.querySelector("#price").innerHTML;
+
+    productCount = el.querySelector("#product_count").value;
+
+    sum += (price.substr(1, price.length) * productCount);
+});
+
+console.log(sum);
+
+subtotal = document.querySelector("#subtotal");
+subtotal.innerHTML = '$' + sum;
