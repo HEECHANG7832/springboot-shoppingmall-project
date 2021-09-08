fd36f657fc24390a910f3530abfc7e7848db9dad
heechang.kwon <facee22av@naver.com>
2021-09-08 18:32:53 +0900

    modify Serializable error

--- src/main/java/com/example/springbootshoppingmallproject/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/springbootshoppingmallproject/config/auth/dto/SessionUser.java
@@ -13,6 +13,8 @@ public class SessionUser implements Serializable {
     //인증된 사용자의 정보만 필요
     //User클래스를 바로 사용할 수 없는 이유는 Entity인 User클래스를 직렬화 할 수 없기 때문
     //Entity는 다른 Entity와 관계가 생길 수 있기 때문에 성능 이슈상 직렬화 불가
+
+    private static final long serialVersionUID = 4334401891246177689L;
     private Long id;
     private String name;
     private String email;
