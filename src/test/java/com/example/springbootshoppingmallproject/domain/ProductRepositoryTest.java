c775100ebe965dfcd5e707f5b124c569069b7ea1
heechang.kwon <facee22av@naver.com>
2021-10-11 20:25:55 +0900

    Test 추가 largeCategory 검색

--- src/test/java/com/example/springbootshoppingmallproject/domain/ProductRepositoryTest.java
+++ src/test/java/com/example/springbootshoppingmallproject/domain/ProductRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.example.springbootshoppingmallproject.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,7 +21,26 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+//    Page<Product> findByLargeCategory(String largeCategory, Pageable pageable);
+//    Page<Product> findAll(Pageable pageable);
+//    List<Product> findTop8ByOrderByPurchaseCountDesc();
+//    Page<Product> findByProductNameContains(String productName, Pageable pageable);
+//    List<Product> findAllByUserId(Long userId);
+// given, when, then
+
+    @DisplayName("대분류 검색 쿼리 테스트")
     @Test
-    public void findByCategoryTest() { // create, read, update, delete
+    public void findByLargeCategoryTest() {
+
+        //given
+        String largeCategory = "Mark Rothko";
+
+        //when
+        int realPage = 0;
+        Pageable pageable = PageRequest.of(realPage, 8);
+        Page<Product> products = productRepository.findByLargeCategory(largeCategory, pageable);
+
+        //then
+        assertThat(products.getContent().get(0).getLargeCategory()).isEqualTo(largeCategory);
     }
 }
